88ca8dd6a1560ae2d025670aad242d061d6c7e3e
Nathan N <Nitharios@users.noreply.github.com>
2018-03-15 19:05:15 -1000

    added rest of script

--- app.py
+++ app.py
@@ -14,3 +14,22 @@ test_headlines = headlines[8000:]
 
 train_labels = labels[:8000]
 test_labels = labels[8000:]
+
+# Create a vectorizer and classifier
+vectorizer = TfidfVectorizer()
+svm = LinearSVC()
+
+# Transform our text data into numerical vectors
+train_vectors = vectorizer.fit_transform(train_headlines)
+test_vectors = vectorizer.transform(test_headlines)
+
+# Train the classifier and predict on test set
+svm.fit(train_vectors, train_labels)
+
+# Generate predictions based on test set
+predictions = svm.predict(test_vectors)
+
+# print(test_headlines[0:5])
+# print(predictions[:5])
+
+print(accuracy_score(test_labels, predictions))
